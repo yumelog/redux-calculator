7857493f9f74dd5e99774124c3466d35795061aa
yumelog <felicita.amore.u@gmail.com>
2020-04-18 21:28:05 +0900

    create store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import CalculatorContainer from './containers/CalculatorContainer';
+import reducer from './reducers';
 import './index.css';
-import CalculatorContainer from './containers/CalculatorContainer'
-import './index.css'
 
-ReactDOM.render(
-  <CalculatorContainer />,
+const store = createStore(reducer);
+
+render(
+  <Provider store={store}>
+    <CalculatorContainer />
+  </Provider>,
   document.getElementById('root')
 );
